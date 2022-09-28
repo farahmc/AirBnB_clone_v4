a665c23ae2befb91cd685744fec1776bea9c8666
farahmc <farah.mccurdy@gmail.com>
2022-09-28 15:27:36 +1000

    add: checkbox list

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,12 @@
 // only load when document is ready
 // listen for changes on each input checkbox tag
-var checkboxes = document.querySelectorAll("input[type=checkbox][name=amenity.name]");
-let enabledSettings = []
 
 document.ready(function() {
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
-          enabledSettings = 
-            Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
-            .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
-            .map(i => i.id) // Use Array.map to extract only the checkbox id from the array of objects.
-            
-          $('div.amenities h4').text(enabledSettings);
+  var amenity_list = [];
+  $.each($("input[name='amenity.name']:checked"), function(){
+      amenity_list.push($(this).val());
+  });
+  
+  $('div.amenities h4').text(amenity_list.join(', '));
     
 });
