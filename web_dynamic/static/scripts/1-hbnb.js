0616decade3b9cb3215a9c66947777d40295385e
farahmc <farah.mccurdy@gmail.com>
2022-09-28 16:30:26 +1000

    add: display amenity list

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,15 +4,18 @@
 document.ready(function() {
   let amenity_dict = {};
 
-  $("input:checkbox").bind("click", function(){
-    let amenity_id = $(this).data("id")
-    let amenity_name = $(this).data("name")
+  $('input:checkbox').bind('click', function(){
+    let amenity_id = $(this).data('id')
+    let amenity_name = $(this).data('name')
 
     if (this.checked) {
       amenity_dict[amenity_id] = amenity_name;
     } else {
       delete amenity_dict[amenity_id];
     }
-  });
-  
+
+    let amenity_list = Array.from(Object.values(amenity_dict))
+    $('div.amenities h4').text(amenity_list)
+
+  });  
 });
