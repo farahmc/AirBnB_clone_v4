e5e96fd096769b9fba811682be1c223b239eb727
farahmc <farah.mccurdy@gmail.com>
2022-09-28 16:15:05 +1000

    add: file

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,8 +2,17 @@
 // listen for changes on each input checkbox tag
 
 document.ready(function() {
-  $('input:checkbox[name=amenity.name]').each(function() {    
-    if($(this).is(':checked'))
-      alert($(this).val());
-});
+  let amenity_dict = {};
+
+  $("input:checkbox").bind("click", function(){
+    let amenity_id = $(this).data("id")
+    let amenity_name = $(this).data("name")
+
+    if (this.checked) {
+      amenity_dict[amenity_id] = amenity_name;
+    } else {
+      delete amenity_dict[amenity_id];
+    }
+  });
+  
 });
