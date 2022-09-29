3a8081aa25b4821f0cbc3e64f431363da7950257
farahmc <farah.mccurdy@gmail.com>
2022-09-29 14:50:30 +1000

    remove: success

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
     $.ajax({
         type: "POST",
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        url: 'http://f6006146b17.9f7fc3ee.hbtn-cod.io:5001/api/v1/places_search/',
         data: '{}',
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
-        success: success
     }).done(function (response) {
         alert( "Data Saved: " + response );
       });      
