b05664b0e5e1feb659da9e2ecdc31e907d5e1d50
farahmc <farah.mccurdy@gmail.com>
2022-09-29 14:53:52 +1000

    changed to local server

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $.ajax({
         type: "POST",
-        url: 'http://f6006146b17.9f7fc3ee.hbtn-cod.io:5001/api/v1/places_search/',
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
         data: '{}',
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
