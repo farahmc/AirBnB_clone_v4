6741e417dab074fa927ff9ae67e776c7107cd8ea
farahmc <farah.mccurdy@gmail.com>
2022-09-29 12:12:17 +1000

    remove hardcoded port

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -46,4 +46,12 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True, debug=True)
