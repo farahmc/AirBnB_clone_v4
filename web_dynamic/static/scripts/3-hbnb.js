4fa88e6a7b4a4fc4b2eb6d62a80c30310f15beb4
Karoline-S <karoline.amy@gmail.com>
2022-09-30 04:08:02 +0000

    perf - remove extra lines

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -69,18 +69,3 @@ $(document).ready(function () {
     $('.amenities h4').text(amenity_list);
   });
 });
-
-
-
-$(document).ready(function () {
-    $.ajax({
-        type: "POST",
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-        data: '{}',
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-    }).done(function (response) {
-        alert( "Data Saved: " + response );
-      });      
-});
-  
