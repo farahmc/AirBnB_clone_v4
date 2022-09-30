33935b37b4dc4952fbecdac74b92e03ed90d7355
Jql11 <jaqueling@126.com>
2022-09-30 08:19:48 +0000

    edit file for task 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,65 +1,7 @@
 $(document).ready(function () {
-  let amenity_dict = {};
-  function places_search_request() {
-      $.ajax({
-	  url: 'http://3ddcb621f547.3ebb58fb.hbtn-cod.io:5001/api/v1/places_search/',
-	  type: 'post',
-	  contentType: 'application/json; charset=utf-8',
-	  dataType: 'json',
-	  data: '{}',
-      })
-	.done((places) => {
-	    const section_places = $('section.places').first();
-	    const articles = [];
-	    for (let place of places) {
-		let article = $('<article>');
-		let div_title_box = $('<div>', {
-		    "class": "title_box"
-		});
- 		let h2_place_name = $('<h2>', {
-		    text: place.name
-		});
-		let div_price_by_night = $('<div>', {
-		    "class": "price_by_night",
-		    text: "$" + place.price_by_night
-		});
-		let div_place_info = $('<div>', {
-		    "class": "information"
-		});
-		let div_max_guest = $('<div>', {
-		    "class": "max_guest",
-		    text: place.max_guest + " Guests"
-		});
-		let div_num_rooms = $('<div>', {
-		    "class": "number_rooms",
-		    text: place.number_rooms + " Bedrooms"
-		});
-		let div_num_bathrooms = $('<div>', {
-		    "class": "number_bathrooms",
-		    text: place.number_bathrooms + "Bathrooms"
-		});
-		let div_place_description = $('<div>', {
-		    'class': 'description',
-		    text: place.description
-		});
-
-		div_title_box.html(h2_place_name);
-		h2_place_name.after(div_price_by_night);
-		article.html(div_title_box);
-		articles.push(article);
-		div_place_info.html(div_max_guest);
-		div_max_guest.after(div_num_rooms);
-		div_num_rooms.after(div_num_bathrooms);
-		div_title_box.after(div_place_info);
-		div_place_info.after(div_place_description)
-	    };
-	    section_places.html(articles);
-	});
-  };
-
-  // get status of api
+//displaying the red dot
   $.ajax({
-    url:'http://3ddcb621f547.3ebb58fb.hbtn-cod.io:5001/api/v1/status',
+    url:'http://1f025ef50d52.9702fef3.hbtn-cod.io:5001/api/v1/status',
     type: 'get',
     dataType: 'json'
   })
@@ -71,10 +13,51 @@ $(document).ready(function () {
     };
   });
 
-  // retrieve and display places from places_search api endpoint
-  places_search_request();
 
-  // create dictionary of checked amenities and display in list
+//displaying ALL places from front-end
+  $.ajax({
+    url: 'http://1f025ef50d52.9702fef3.hbtn-cod.io:5001/api/v1/places_search/',
+    type: 'post',
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    data: '{}',
+  })
+  .done((places) => {
+    const section_places = $('section.places').first();
+    const articles = [];
+    for (let place of places) {
+	let article = $('<article>');
+	let div_title_box = $('<div>', {"class": "title_box"});
+ 	let h2_place_name = $('<h2>', {text: place.name});
+	let div_price_by_night = $('<div>', {
+	    "class": "price_by_night",
+	    text: "$" + place.price_by_night
+	});
+	div_title_box.html(h2_place_name);
+	h2_place_name.after(div_price_by_night);
+	article.html(div_title_box);
+	articles.push(article);
+
+	let div_place_info = $('<div>', {"class": "information"});
+	let div_max_guest = $('<div>', {"class": "max_guest", text: place.max_guest + " Guests"});
+	let div_num_rooms = $('<div>', {"class": "number_rooms", text: place.number_rooms + " Bedrooms"});
+	let div_num_bathrooms = $('<div>', {"class": "number_bathrooms", text: place.number_bathrooms + "Bathrooms"});
+	div_place_info.html(div_max_guest);
+	div_max_guest.after(div_num_rooms);
+	div_num_rooms.after(div_num_bathrooms);
+	div_title_box.after(div_place_info);
+
+	const regex = /<br[^>]*>/gi
+	let div_place_description = $('<div>', {'class': 'description', text: place.description});
+	div_place_info.after(div_place_description)
+    } 
+    section_places.html(articles);
+  });
+
+
+//dispalying checkbox with each checked item
+  let amenity_dict = {};
+
   $('input[type=checkbox]').bind('click', function () {
     let amenity_id = $(this).data('id')
     let amenity_name = $(this).data('name')
@@ -90,9 +73,27 @@ $(document).ready(function () {
     }).sort().join(', ');
     $('.amenities h4').text(amenity_list);
   });
+  
 
-  // request places_search api endpoint when search button is clicked
-  $('<button>').click(function() {
-    alert( "Handler for .click() called." );
+// request places_search api endpoint when search button is clicked
+  $("button").click(function(){
+       $.ajax({
+          url: 'http://1f025ef50d52.9702fef3.hbtn-cod.io:5001/api/v1/places_search',
+          type: 'post',
+          contentType: 'application/json; charset=utf-8',
+          dataType: 'json',
+          data: JSON.stringify({'amenities': Object.keys(amenity_dict)}),
+      })
+        .done((places) => {
+          console.log(places);
+          const section_places = $('section.places').first();
+          const articles = [];
+          for (let place of places) {
+            let article = $('<article>');
+            article.html("<div class='title_box'><h2>" + place.name + "</h2><div class='price_by_night'>" + place.price_by_night + "</div></div><div class='information'><div class='max_guest'>" + place.max_guest + " Guest</div><div class='number_rooms'>" + place.number_rooms + " Bedroom</div><div class='number_bathrooms'>" + place.number_bathrooms + " Bathroom</div></div><div class='description'>" + place.description + "</div>");
+            articles.push(article);
+            }
+          section_places.html(articles);
+        });
   });
 });
